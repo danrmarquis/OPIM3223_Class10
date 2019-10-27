2cfffaa65aa2e27d721ad883938efaba6a9c02a9
Dan Marquis <daniel.marquis@uconn.edu>
2019-10-27 16:16:58 -0400

    Initial class commit

---
+++ Class10/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public Animator startButton;
+    public Animator settingsButton;
+    public Animator dialog;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void OpenSettings()
+    {
+        startButton.SetBool("isHidden", true);
+        settingsButton.SetBool("isHidden", true);
+        dialog.SetBool("isHidden", false);
+    }
+
+    public void CloseSettings()
+    {
+        startButton.SetBool("isHidden", false);
+        settingsButton.SetBool("isHidden", false);
+        dialog.SetBool("isHidden", true);
+    }
+}
